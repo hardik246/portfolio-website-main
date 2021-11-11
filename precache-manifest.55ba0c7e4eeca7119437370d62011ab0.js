8435f1d90b96fcbb4063a286f2a9f7026feacafe
Hardik Thakkar <hardik.thakkar@artoon.in>
2021-11-11 15:24:52 +0530

    Updates

---
+++ precache-manifest.55ba0c7e4eeca7119437370d62011ab0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c2676fd595d9dfa773fe11a9610bc9",
+    "url": "/portfolio-website-main/index.html"
+  },
+  {
+    "revision": "1c61e0ea748c508766d3",
+    "url": "/portfolio-website-main/static/css/main.4dffe875.chunk.css"
+  },
+  {
+    "revision": "12c82ef9895caa469274",
+    "url": "/portfolio-website-main/static/js/2.2725c7e4.chunk.js"
+  },
+  {
+    "revision": "5bc25f0f1f9fc74e0370e142e3be15b5",
+    "url": "/portfolio-website-main/static/js/2.2725c7e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c61e0ea748c508766d3",
+    "url": "/portfolio-website-main/static/js/main.4ded7d18.chunk.js"
+  },
+  {
+    "revision": "7bf7900f0074291fc1b5",
+    "url": "/portfolio-website-main/static/js/runtime-main.c15afe80.js"
+  },
+  {
+    "revision": "040734040e0f2f40b699334a2f5734d0",
+    "url": "/portfolio-website-main/static/media/hardikThakkar.04073404.jpeg"
+  }
+]);
