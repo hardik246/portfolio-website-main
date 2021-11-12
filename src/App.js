a41da4cb60a0f4d278efb202bcc6be44a00c7e43
Hardik Thakkar <hardik.thakkar@artoon.in>
2021-11-12 17:07:41 +0530

    Create a React app and publish

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-} from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './contents/Home';
-import About from './contents/About';
-import Education from './contents/Education';
-import Skills from './contents/Skills';
-import Contact from './contents/Contact';
-
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./contents/Home";
+import About from "./contents/About";
+import Education from "./contents/Education";
+import Skills from "./contents/Skills";
+import Contact from "./contents/Contact";
 
 function App() {
   return (
-    <Router>
-    <div className="App">
-    <Navbar />
-    <Route exact path="/">
-    <Home />
-    </Route>
-    <Route path="/about">
-    <About />
-    </Route>
-    <Route path="/education">
-    <Education />
-    </Route>
-    <Route path="/skills">
-    <Skills />
-    </Route>
-    <Route path="/contact">
-    <Contact />
-    </Route>
+    <div className="container-fluid">
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/education">
+            <Education />
+          </Route>
+          <Route path="/skills">
+            <Skills />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </div>
+      </Router>
     </div>
-    </Router>
-    );
-  }
-  
-  export default App;
-  
+  );
+}
+
+export default App;
