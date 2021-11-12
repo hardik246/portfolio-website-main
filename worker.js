13a702967ca120eff2b2a0ae94ee16d63cfcdf79
Hardik Thakkar <hardik.thakkar@artoon.in>
2021-11-12 18:11:47 +0530

    Updates

---
+++ worker.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = "portfolio-website-main";
+var urlsToCache = [
+  "/static/js/main.chunk.js",
+  "/static/js/0.chunk.js",
+  "/static/js/bundle.js",
+  "/index.html",
+  "/",
+  "/completed",
+];
+
+// Install a service worker
+self.addEventListener("install", (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Cache and return requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Update a service worker
+self.addEventListener("activate", (event) => {
+  var cacheWhitelist = ["portfolio-website-main"];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
